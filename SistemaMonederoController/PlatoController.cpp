57cd17e8ccf3746429e74a5498770bd672c82d16
thoniid <anthony.valladolid@pucp.edu.pe>
2023-11-13 17:38:47 -0500

    Revert "uu"
    This reverts commit ab80ae22d8bd5e09095788810d79c49fcd703444.

--- SistemaMonederoController/PlatoController.cpp
+++ SistemaMonederoController/PlatoController.cpp
@@ -198,7 +198,12 @@ List<String^>^ PlatoController::obtenerOrigenes() {
     return listaOrigenes;
 }
 
-// IMPLEMENTACIONES EN BASE DE DATOS
+
+
+
+
+// IMPLEMENTACIONES EN BASE DE DATOS 
+
 PlatoController::PlatoController() {
     this->objConexion= gcnew SqlConnection(); 
 
