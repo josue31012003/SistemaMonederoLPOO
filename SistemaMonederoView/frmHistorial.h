baa6fae9fae2fd1f26ae180bd8fbb7faed9e1c9f
Valerie Cabrera Mays <a20213391@pucp.edu.pe>
2023-12-09 17:44:28 -0500

    load borrado, no se usaba

--- SistemaMonederoView/frmHistorial.h
+++ SistemaMonederoView/frmHistorial.h
@@ -257,26 +257,6 @@ namespace SistemaMonederoView {
 		}
 	}
 
-	private: System::Void frmHistorial_Load(System::Object^ sender, System::EventArgs^ e) {
-		int columnIndex = 1; // Indica el índice de la columna que quieres analizar
-
-		int maxTextLength = 0;
-
-		if (textBox6->Text != "") {
-			for (int i = 0; i < dataGridView1->Rows->Count; i++) {
-				String^ text = dataGridView1->Rows[i]->Cells[columnIndex]->Value->ToString();
-				int length = text->Length;
-				if (length > maxTextLength) {
-					maxTextLength = length;
-				}
-			}
-
-			// Establece el ancho de la columna en función de la longitud máxima encontrada
-			dataGridView1->Columns[columnIndex]->Width = maxTextLength * 10; // Ajusta someFactor según tu preferencia
-		}
-
-	}
-
 	private: List<PlatoVendido^>^ mostrarGrilla(List<Transaccion^>^ listTransacciones) {
 		this->dataGridView1->Rows->Clear(); /* Elimino toda la información de la grilla1 */
 		List<PlatoVendido^>^ listaPlatosVendidos = nullptr;
